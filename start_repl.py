fd70dc83e2d5219fa69ce4815a0e70ce62fb3637
ntEOS <nguyentoan231196@gmail.com>
2024-02-28 20:12:06 +0700

    Ensure the repl to work and user must input

--- start_repl.py
+++ start_repl.py
@@ -1,10 +1,17 @@
+import os
+
+
 def start_repl():
     print(
         """
-    (c) Copy right nt2311-vn
+(c) Copy right nt2311-vn
     All right reserved.
-    Welcome to the check diff fulfillment program. Type ? or help to list commands
-
-    """
+    Welcome to the check diff fulfillment program. Type ? or help to list commands"""
     )
-    print(">")
+    while True:
+        user_input = input("> ")
+
+        if not user_input:
+            continue
+        else:
+            os._exit(0)
