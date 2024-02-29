514649a4db1a884f1103166e82ee6430e7011da8
nt2311-vn <nguyentoan231196@gmail.com>
2024-02-29 16:50:37 +0700

    Complete the check data

--- repl/command_checkdata.py
+++ repl/command_checkdata.py
@@ -6,23 +6,34 @@ def command_checkdata():
     num_files = [f for f in os.listdir("./data/") if f not in [".gitkeep"]]
 
     if len(num_files) != 2:
-        raise Exception("Required exactly two files: IF and WO in the files folder")
+        print("Required exactly two files: IF and WO in the files folder")
 
     list_filetypes = []
     list_file_created_date = []
+    list_file_name = []
 
     for file in num_files:
         list_filetypes.append(file.split(".")[1])
+        list_file_name.append(file.split(".")[0])
         timestamp = os.path.getmtime(f"./data/{file}")
         date_time = datetime.fromtimestamp(timestamp)
 
-        list_file_created_date.append(date_time.strftime("%d/%m/%Y %H:%M:%S"))
+        list_file_created_date.append(date_time)
 
     if not all([filetype == "csv" for filetype in list_filetypes]):
-        raise Exception("Your input in the data folder must be in csv format")
+        print("Your input in the data folder must be in csv format")
 
-    print("Your data is good to go, type start to begin analyzing data")
+    if not all([file_name in ["IF", "WO"] for file_name in list_file_name]):
+        print("Your data must be name IF and WO for name convention")
 
+    # If min_date is not to day print to awareness
     min_date = min(list_file_created_date)
-    print(f"Once of your data was downloaded at {min_date}")
-    print("")
+    if min_date.strftime("%Y-%m-%d") != datetime.now().strftime("%Y-%m-%d"):
+        print(
+            f"WARNING: Once of your data is downloaded on {min_date.strftime("%d/%m/%Y %H:%M:%S")}, please download new one or begin the program by typing start"
+        )
+        print("")
+
+    else:
+        print("Your data is good to go, type start to begin analyzing data")
+        print("")
