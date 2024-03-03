5d8f4163b460c45da6963aef5b6e33b1cdc4d23c
ntEOS <nguyentoan231196@gmail.com>
2024-03-03 23:07:25 +0700

    remove the color from bar grahic due to windown or bare cmd did not support color

--- graphics/pacman_bar.py
+++ graphics/pacman_bar.py
@@ -6,6 +6,8 @@ def pacman_bar(current_index, total_number):
     fill_char = "="
     lead_char = ">"
 
+    # How to give this bar a color
+
     progress = current_index / total_number
     block = int(round(bar_length * progress))
 
@@ -14,7 +16,6 @@ def pacman_bar(current_index, total_number):
     else:
         bar = fill_char * (block - 1) + lead_char + "-" * (bar_length - block)
 
-    text = f"\r\033[0;32;40mProgress:[{bar}] {progress * 100:.2f}%\033[0m"
-
+    text = f"\rProgress:[{bar}] {progress * 100:.2f}%"
     sys.stdout.write(text)
     sys.stdout.flush()
